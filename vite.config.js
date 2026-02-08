5513e74abf3f219aba7c8fcbcb2cc28412011d2a
agrimajn <agrimajn@gmail.com>
2026-02-08 13:31:37 -0500

    tailwind changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+plugins : [react(), tailwindcss()]
 
 // https://vite.dev/config/
 export default defineConfig({
